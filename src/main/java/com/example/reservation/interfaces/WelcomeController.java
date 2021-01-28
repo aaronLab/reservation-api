3da3fce7e4f542e71dd90f050f59de652709ab1c
Aaron Lee <aaronlab.net@gmail.com>
2021-01-28 21:09:39 +0900

    hello world

---
+++ src/main/java/com/example/reservation/interfaces/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example.reservation.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
+
+}
